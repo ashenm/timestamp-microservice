1f406ad097f1e549c6e9a6f659d7f2f0dc669ac3
Ashen Gunaratne <mail@ashenm.ml>
2017-11-21 11:08:58 +0530

    Implement in vanilla NodeJS

--- server.js
+++ server.js
@@ -7,33 +7,48 @@
  *
  */
 
-const express = require('express');
-const app = express();
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-// static files
-app.use(express.static('public'));
+http.createServer((request, response) => {
 
-// timestamp microservice
-app.all('/:time', (request, response) => {
+  // only allow GET requests
+  if (!/GET/.test(request.method)) {
+    response.writeHead(405, {'Content-Type': 'text/plain'});
+    response.end(response.statusMessage);
+    return;
+  }
+
+  // index
+  if (/^\/$/.test(request.url)) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(path.join(__dirname, 'public', 'index.html')).pipe(response);
+    return;
+  }
+
+  // favicon
+  if (/^\/favicon\.ico$/.test(request.url)) {
+    response.writeHead(404);
+    response.end(null);
+    return;
+  }
 
   let date;
 
+  // extract input
+  const input = decodeURIComponent(request.url.replace(/^\//, ''));
+
   // process as natural language if non numeric
   // character included else fallback to unix
-  (/\D/.test(request.params.time))
-    ? date = new Date(request.params.time)
-    : date = new Date(parseInt(request.params.time, 10) * 1000);
+  /\D/.test(input)
+    ? date = new Date(input)
+    : date = new Date(parseInt(input, 10) * 1000);
 
-  (date.toString() === 'Invalid Date')
-    ? response.json({
-        unix: null,
-        natural: null
-      })
-    : response.json({
-        unix: date.getTime() / 1000,
-        natural: date.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'})
-      });
+  date.toString() === 'Invalid Date'
+    ? (response.end(JSON.stringify({unix: null, natural: null})),
+        response.writeHead(400, {'Content-Type': 'application/json'}))
+    : (response.end(JSON.stringify({unix: date.getTime() / 1000, natural: date.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: '2-digit'})})),
+        response.writeHead(200, {'Content-Type': 'application/json'}));
 
-});
-
-app.listen(process.env.PORT || 8080);
+}).listen(process.env.PORT || 8080);
